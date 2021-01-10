e159e03bceb584fde0cd74ac6cf8312a318315b2
puremoru <e155745@ie.u-ryukyu.ac.jp>
2021-01-11 00:40:09 +0900

    :+1: APIからデータ取得

--- pages/hoge.js
+++ pages/hoge.js
@@ -1,7 +1,32 @@
-export default function Hoge() {
+import Fetch from 'isomorphic-unfetch';
+
+function Hoge({ stars }) {
+    const foo = 'bar'
+    const arr = ['first', 'second', 'third']
+    const numberList = []
+
+    for (let a of arr) {
+        numberList.push(<li key={a}>{a}</li>)
+    }
     return (
         <div>
             <h2>Hello Hoge</h2>
+            <p>foo: {foo}</p>
+            <div>
+                <p>配列をループで表示させてるよ</p>
+                <ul>
+                    {numberList}
+                </ul>
+            </div>
+            <p>APIから取得したデータ: {stars}</p>
         </div>
     )
 }
+
+Hoge.getInitialProps = async ({ req }) => {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+  }
+
+export default Hoge
